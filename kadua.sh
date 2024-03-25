a69fcb98e97c1479af29c915e6532584e03cca89
ladora <ladora@qdeathse.com>
2024-03-25 15:03:21 +0000

    kadua.sh edited online with Bitbucket

--- kadua.sh
+++ kadua.sh
@@ -1,5 +1,5 @@
 gcloud alpha ai custom-jobs create \
     --region=europe-north1 \
     --display-name=kucluk1 \
-    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
