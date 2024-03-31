76d0d153945c49e0b51e8d2cc9ea57b32a84ef26
ladora <ladora@qdeathse.com>
2024-03-31 14:16:58 +0000

    mimiti.sh edited online with Bitbucket

--- mimiti.sh
+++ mimiti.sh
@@ -1,105 +1,105 @@
 gcloud alpha ai custom-jobs create \
     --region=us-central1 \
     --display-name=kucluk1 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=us-east1 \
     --display-name=kucluk2 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=us-east4 \
     --display-name=kucluk3 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=us-south1 \
     --display-name=kucluk4 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=us-west1 \
     --display-name=kucluk5 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=us-west2 \
     --display-name=kucluk6 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=us-west3 \
     --display-name=kucluk7 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=us-west4 \
     --display-name=kucluk8 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=asia-east1 \
     --display-name=kucluk9 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=asia-northeast1 \
     --display-name=kucluk10 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=asia-south1 \
     --display-name=kucluk11 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=asia-southeast1 \
     --display-name=kucluk12 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=australia-southeast1 \
     --display-name=kucluk13 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=europe-central2 \
     --display-name=kucluk14 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=europe-north1 \
     --display-name=kucluk15 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=europe-west1 \
     --display-name=kucluk16 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=europe-west2 \
     --display-name=kucluk17 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=europe-west3 \
     --display-name=kucluk18 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=europe-west4 \
     --display-name=kucluk19 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=me-west1 \
     --display-name=kucluk20 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
 gcloud alpha ai custom-jobs create \
     --region=southamerica-west1 \
     --display-name=kucluk21 \
-    --worker-pool-spec=machine-type=e2-standard-32,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
-    --worker-pool-spec=machine-type=e2-standard-8,replica-count=1,container-image-uri=gcr.io/wik/subu:nya
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=1,container-image-uri=gcr.io/wik/subu:nya \
+    --worker-pool-spec=machine-type=e2-standard-4,replica-count=9,container-image-uri=gcr.io/wik/subu:nya
